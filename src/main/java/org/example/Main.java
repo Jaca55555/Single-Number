c3f4e47be3259531bc9461fdfe7aa56401a72d96
jaca55555 <jahongitToshpulatov@mail.ru>
2023-08-17 17:29:01 +0500

    sortPeople

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,33 +10,31 @@ import java.util.stream.IntStream;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public boolean isHappy(int n) {
-           int a=n;
-           int b=n;
-           do {
-               a=square(a);
-               b=square(square(b));
-           }while (a!=b);
-           return a==1;
+    public String[] sortPeople(String[] names, int[] heights) {
 
-    }
-    public int square(int num) {
-
-        int ans = 0;
-
-        while(num > 0) {
-            int remainder = num % 10;
-            ans += remainder * remainder;
-            num /= 10;
+        String  max;
+        int maInt;
+        for (int i = 0; i <heights.length ; i++) {
+            for (int j = i+1; j <heights.length ; j++) {
+                if (heights[i]<heights[j]){
+                    maInt=heights[i];
+                    heights[i]=heights[j];
+                    heights[j]=maInt;
+                    max=names[i];
+                    names[i]=names[j];
+                    names[j]=max;
+                }
+            }
         }
-
-        return ans;
+        return names;
+        
     }
-
     public static void main(String[] args) {
         Main main = new Main();
-        int n=19;
-        System.out.println(main.isHappy(n));
+
+        String [] names={"Mary","John","Emma"};
+        int[] heights={180,165,170};
+        System.out.println(main.sortPeople(names,heights));
 
 
     }

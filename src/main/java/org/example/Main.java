a660830df3dd5ed167c40db36e9cdd6e4a1994e0
jaca55555 <jahongitToshpulatov@mail.ru>
2023-08-18 10:53:23 +0500

    Pow

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,40 +1,21 @@
 package org.example;
 //A=65
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 import java.util.stream.IntStream;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public String[] sortPeople(String[] names, int[] heights) {
+    public double myPow(double x, int n) {
+        return Math.pow(x,n);
 
-        String  max;
-        int maInt;
-        for (int i = 0; i <heights.length ; i++) {
-            for (int j = i+1; j <heights.length ; j++) {
-                if (heights[i]<heights[j]){
-                    maInt=heights[i];
-                    heights[i]=heights[j];
-                    heights[j]=maInt;
-                    max=names[i];
-                    names[i]=names[j];
-                    names[j]=max;
-                }
-            }
-        }
-        return names;
-        
     }
     public static void main(String[] args) {
         Main main = new Main();
-
-        String [] names={"Mary","John","Emma"};
-        int[] heights={180,165,170};
-        System.out.println(main.sortPeople(names,heights));
+        double x=2.00000;
+        int n=10;
+        System.out.println(main.myPow(x,n));
 
 
     }

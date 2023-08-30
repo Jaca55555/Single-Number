5260bbc8c798c977275b128bed095827cd0802e5
jaca55555 <jahongitToshpulatov@mail.ru>
2023-08-30 13:08:32 +0500

    alternating-digit-sum

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,23 @@
 package org.example;
 
 public class Main {
-    public boolean squareIsWhite(String coordinates) {
-        char[] chars=new char[2];
-        chars[0]=coordinates.charAt(0);
-        chars[1]=coordinates.charAt(1);
-       return chars[0] % 2 == 1 && chars[1] % 2 == 0 || chars[0] % 2 == 0 && chars[1] % 2 == 1;
-    }
+    public int alternateDigitSum(int n) {
+        int a = 0;
+        int sum = 0, i = 1;
+        do {
+            a = n % 10;
+            n = n / 10;
+            i++;
+            sum += Math.pow(-1, i) * a;
+        } while (n > 0);
+        return i % 2 == 0 ? sum : -sum;
 
+    }
 
     public static void main(String[] args) {
         Main main = new Main();
-        String a="c7";
-        System.out.println(main.squareIsWhite(a));
-
+        int a = 521;
+        System.out.println(main.alternateDigitSum(a));
 
 
     }

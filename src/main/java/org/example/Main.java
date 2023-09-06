f307c8404259fea4c53041eed1bcacc15aecd38c
jaca55555 <jahongitToshpulatov@mail.ru>
2023-09-06 18:13:40 +0500

    three-consecutive-odds

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,23 +1,20 @@
 package org.example;
 
 public class Main {
-    public int alternateDigitSum(int n) {
-        int a = 0;
-        int sum = 0, i = 1;
-        do {
-            a = n % 10;
-            n = n / 10;
-            i++;
-            sum += Math.pow(-1, i) * a;
-        } while (n > 0);
-        return i % 2 == 0 ? sum : -sum;
+    public boolean threeConsecutiveOdds(int[] arr) {
+        for (int i = 1; i <arr.length-1 ; i++) {
+            if (arr[i-1]%2==1&&arr[i]%2==1&&arr[i+1]%2==1){
+                return true;
+            }
+        }
+        return false;
 
     }
 
     public static void main(String[] args) {
         Main main = new Main();
-        int a = 521;
-        System.out.println(main.alternateDigitSum(a));
+        int[] a = {2,6,4,1};
+        System.out.println(main.threeConsecutiveOdds(a));
 
 
     }

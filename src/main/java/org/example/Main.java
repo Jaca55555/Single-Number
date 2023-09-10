e7e28c4392fb8dc4a48ed77f4b71965cb4bcd6c4
jaca55555 <jahongitToshpulatov@mail.ru>
2023-09-10 21:52:39 +0500

    count-negative-numbers-in-a-sorted-matrix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,26 +1,20 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
-    public List<Integer> getRow(int rowIndex) {
-
-        List<Integer> list=new ArrayList<>(rowIndex+1);
-        list.add(0,1);
-        for (int i = 1; i <= rowIndex; ++i) {
-            list.add(1);
-            for (int j = i-1; j >0 ; --j) {
-                list.set(j,list.get(j)+list.get(j-1));
+    public int countNegatives(int[][] grid) {
+        int sum=0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+               if (grid[i][j]<0)sum++;
             }
-
-        }return list;
+        }
+        return sum;
     }
 
     public static void main(String[] args) {
         Main main = new Main();
-        int a=4;
-        System.out.println(main.getRow(a));
+        int[][] a={{4, 3, 2, -1}, {3, 2, 1, -1}, {1, 1, -1, -2}, {-1, -1, -2, -3}};
+        System.out.println(main.countNegatives(a));
 
 
     }

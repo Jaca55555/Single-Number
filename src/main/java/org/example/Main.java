0abdf6bfa133626d38881a06950d7aada194faf1
jaca55555 <jahongitToshpulatov@mail.ru>
2023-09-21 15:41:18 +0500

    maximum-number-of-balloons

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,27 +3,33 @@ package org.example;
 import java.util.Arrays;
 
  class Main {
-     public boolean canMakeArithmeticProgression(int[] arr) {
+     public int maxNumberOfBalloons(String text) {
 
-         boolean t = true;
-         Arrays.sort(arr);
+         char[] chars=new char[text.length()];
+         int a=0,b=0, n=0, l=0,o=0, s=0,g=0;
+         text.chars();
+         for (int i = 0; i < chars.length; i++) {
+             switch (text.charAt(i)){
+                 case 'b':  b++; break;
+                 case 'a':  a++; break;
+                 case 'l':  s++;{if (s%2==0)l++;
+                 }; break;
+                 case 'o':  g++;{
+                     if (g%2==0)o++;
+                 }; break;
+                 case 'n':  n++; break;
 
-         for (int i = 0; i < arr.length - 2; i++) {
-             int diff1 = Math.abs(arr[i] - arr[i+1]);
-             int diff2 = Math.abs(arr[i+1] - arr[i+2]);
-
-             if (diff1 != diff2) {
-                 return false;
              }
          }
-
-         return t;
+         int min=0;
+        min= Math.min(Math.min(Math.min(b,a),Math.min(l,o)),n);
+         return min==-1?0:min;
      }
 
     public static void main(String[] args) {
 
         Main main = new Main();
-        int[] arr={3,5,1};
-        System.out.println(main.canMakeArithmeticProgression(arr));
+        String a="krhizmmgmcrecekgyljqkldocicziihtgpqwbticmvuyznragqoyrukzopfmjhjjxemsxmrsxuqmnkrzhgvtgdgtykhcglurvppvcwhrhrjoislonvvglhdciilduvuiebmffaagxerjeewmtcwmhmtwlxtvlbocczlrppmpjbpnifqtlninyzjtmazxdbzwxthpvrfulvrspycqcghuopjirzoeuqhetnbrcdakilzmklxwudxxhwilasbjjhhfgghogqoofsufysmcqeilaivtmfziumjloewbkjvaahsaaggteppqyuoylgpbdwqubaalfwcqrjeycjbbpifjbpigjdnnswocusuprydgrtxuaojeriigwumlovafxnpibjopjfqzrwemoinmptxddgcszmfprdrichjeqcvikynzigleaajcysusqasqadjemgnyvmzmbcfrttrzonwafrnedglhpudovigwvpimttiketopkvqw";
+        System.out.println(main.maxNumberOfBalloons(a));
     }
 }

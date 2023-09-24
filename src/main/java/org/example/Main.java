2e1aa6a08073030a6af71816f1f964f0aa953d07
jaca55555 <jahongitToshpulatov@mail.ru>
2023-09-24 17:52:30 +0500

    find-the-difference

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,22 @@
 package org.example;
 
-class Main {
-    public String reverseOnlyLetters(String s) {
-        char temp[] = s.toCharArray();
-        int low = 0 , high = s.length()-1;
-        while(low < high){
-            if(Character.isAlphabetic(temp[low]) && Character.isAlphabetic(temp[high])){
-                char i = temp[low];temp[low] = temp[high];
-                temp[high] = i;
-                low++; high--;
-            }else if(!Character.isAlphabetic(temp[low]))  low++;
-            else if(!Character.isAlphabetic(temp[high]))  high--;
-        }
-        return String.valueOf(temp);
 
+import java.util.ArrayList;
+import java.util.List;
+
+class Main {
+    public char findTheDifference(String s, String t) {
+        char c = 0;
+        for(char cs : s.toCharArray()) c ^= cs;
+        for(char ct : t.toCharArray()) c ^= ct;
+        return c;
     }
 
     public static void main(String[] args) {
 
         Main main = new Main();
-        String nums ="ab-cd" ;
-        System.out.println(main.reverseOnlyLetters(nums));
+        String a="abcd";
+        String b="abcde";
+        System.out.println(main.findTheDifference(a,b));
     }
 }
